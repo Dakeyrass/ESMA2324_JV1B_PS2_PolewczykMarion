4b5387766112dc771db4fdfaaabd1ca7149c7fc8
Dakeyrass <pwkmarion@gmail.com>
2024-05-21 15:03:02 +0200

    saut
    Le joueur peut sauter.

--- Assets/Scripts/Joueur.cs
+++ Assets/Scripts/Joueur.cs
@@ -6,13 +6,15 @@ public class Joueur : MonoBehaviour
 {
     //STATS
     public float vie;
-    public float poids;
-    public float force;
+    public float vitesse;
+    public float force_saut;
+
     //COMPONENTS
     private Rigidbody2D rgbd;
     private Collider2D col;
-    //DEPLACEMENTS
-    public float vitesse;
+
+    //CONDITIONS
+    private bool au_sol = true;
 
     // Start is called before the first frame update
     void Start()
@@ -23,8 +25,27 @@ public class Joueur : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+        //deplacements basiques 
         float horizontal = Input.GetAxis("Horizontal");
         rgbd.velocity = new Vector2(horizontal * vitesse, rgbd.velocity.y);
+        //saut
+        if (Input.GetKeyDown(KeyCode.Space)&& au_sol == true)
+        {
+            sauter();
+        }
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Sol")
+        {
+            au_sol = true; 
+        }
+    }
+    private void sauter()
+    {
+        rgbd.velocity = new Vector2 (rgbd.velocity.x,force_saut);
+        au_sol = false;
+    }
+
 }
