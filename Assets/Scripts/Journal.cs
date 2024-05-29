c5a53f18cdd05a22d561ef5ce11e84dc031451e7
Dakeyrass <pwkmarion@gmail.com>
2024-05-29 21:06:43 +0200

    ajout journal
    Présente la situation du perso
    Oui c'est totalement la lettre du zelda like, on appelle ça du recyclage.

---
+++ Assets/Scripts/Journal.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Journal : MonoBehaviour
+{
+    public Image journal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        journal.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Affiche_journal();
+    }
+
+    private void Affiche_journal()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            journal.enabled = true; 
+        }
+        else if (Input.GetKeyUp(KeyCode.R))
+        {
+            journal.enabled = false; 
+        }
+
+    }
+}
